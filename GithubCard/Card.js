ae999f6414f45efe0ab7e77f040e1f649081266d
Austin Akers <austin.akers5@gmail.com>
2017-12-28 23:28:45 -0600

    Completed CardList

--- GithubCard/Card.js
+++ GithubCard/Card.js
@@ -11,25 +11,67 @@ const Card = (props) => {
   );
 };
 
-let data = [
-	{
-  	name: "Austin Akers",
-    avatar_url: "https://avatars1.githubusercontent.com/u/11778717?v=4",
-    company: "Ambit Energy"
-  },
-  {
-  	name: "Alex Akers",
-    avatar_url: "https://avatars1.githubusercontent.com/u/11778717?v=4",
-    company: "Airforce"
-  },
-]
-
 
 const CardList = (props) => {
 	return (
   	<div>
     	{props.cards.map(card => <Card {...card} />)}
     </div>
-  )
+  );
 }
-ReactDOM.render(<CardList cards={data} />, mountNode);
+
+class Form extends React.Component {
+	state = { userName: ''}
+	handleSubmit = (event) => {
+  	event.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+    	.then(Result => {
+      	this.props.onSubmit(Result.data);
+        this.setState({ userName: ''});
+      });
+  };
+	render() {
+  	return (
+    	<form onSubmit={this.handleSubmit}>
+      	<input type="text" 
+      	value={this.state.userName}
+        onChange={(event) => this.setState({userName: event.target.value})}
+        placeholder="Github username" />
+        <button type="submit">Add card</button>
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+	state = {
+  	cards: [
+    	{
+        name: "Austin Akers",
+        avatar_url: "https://avatars1.githubusercontent.com/u/11778717?v=4",
+        company: "Ambit Energy"
+      },
+      {
+        name: "Alex Akers",
+        avatar_url: "https://avatars1.githubusercontent.com/u/11778717?v=4",
+        company: "Airforce"
+      },
+    ]
+  };
+  
+  addNewCard = (cardInfo) => {
+  this.setState(prevState => ({
+  	cards: prevState.cards.concat(cardInfo)
+  }));
+  };
+  
+	render() {
+  	return (
+    	<div>
+      	<Form onSubmit={this.addNewCard}/>
+        <CardList cards={this.state.cards}/>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<App />, mountNode);
