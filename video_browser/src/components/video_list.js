ea5a8c2ccccbbfc15f8b2f4cf4bd76e4d7e1f25a
Austin Akers <austin.akers5@gmail.com>
2018-05-03 10:11:11 -0500

    created youtube fetch call and rendering video list

---
+++ video_browser/src/components/video_list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} key={video.id.videoId} />
+  });
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  );
+};
+
+export default VideoList;
