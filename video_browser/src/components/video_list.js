b52e402eabdba0aa1592d45d0389a6bcf503acb0
Austin Akers <austin.akers5@gmail.com>
2018-05-03 16:00:51 -0500

    finished youtube search engine project

--- video_browser/src/components/video_list.js
+++ video_browser/src/components/video_list.js
@@ -2,7 +2,12 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} key={video.id.videoId} />
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        video={video}
+        key={video.id.videoId} />
+    )
   });
   return (
     <ul className="col-md-4 list-group">
