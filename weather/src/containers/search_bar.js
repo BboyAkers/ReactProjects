714552ba3f65ed9a14970024875b4f195291dff9
Austin Akers <austin.akers5@gmail.com>
2018-05-08 12:08:03 -0500

    added request and preventDefault on form submission

--- weather/src/containers/search_bar.js
+++ weather/src/containers/search_bar.js
@@ -6,11 +6,23 @@ export default class SearchBar extends Component {
     super(props);
 
     this.state = {term: ''};
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+  onInputChange(event) {
+    this.setState({term: event.target.value}); 
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
   }
   render() {
     return (
-      <form className="input-group">
-        <input placeholder="Choose city for 5 day forcast" />
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input 
+          placeholder="Choose city for 5 day forcast"
+          value={this.state.term}
+          onChange={this.onInputChange} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
