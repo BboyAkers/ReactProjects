c95a27bce9b92210e60c041d6f5882fed28b9b14
Austin Akers <austin.akers5@gmail.com>
2018-05-09 11:24:46 -0500

    refactored and re-implimented charts

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -5,7 +5,7 @@ const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${A
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`
+  const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
   return {
     type: FETCH_WEATHER,
