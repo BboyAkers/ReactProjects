4f98a11101aec30e17d8355e3200844bb57d07b4
Austin Akers <austin.akers5@gmail.com>
2018-05-14 11:59:13 -0500

    added axios call and posts_index.js

---
+++ blog/src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+  render () {
+    return (
+      <div>
+        Post Index
+      </div>
+    )
+  }
+}
+
+export default PostsIndex;
--- blog/src/index.js
+++ blog/src/index.js
@@ -2,13 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import PostIndex from './components/posts_index';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.querySelector(".container")
 );
