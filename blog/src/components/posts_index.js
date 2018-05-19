7ce32d431739968500b150e6b99f6813d39625a5
Austin Akers <austin.akers5@gmail.com>
2018-05-19 12:08:57 -0500

    added link to add a post,imported and created form for adding a new post

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import _ from 'lodash';
 import { fetchPosts } from '../actions';
 
@@ -20,7 +21,11 @@ class PostsIndex extends Component {
   render () {
     return (
       <div>
-        <h3>Posts</h3>
+        <div className="text-xs-right"> 
+        <Link className="btn btn-primary" to="/posts/new">
+          Add a Post
+        </Link>
+        </div>
         <ul className="list-group">
           {this.renderPosts()}
         </ul>
