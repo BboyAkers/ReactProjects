103bc0b1dd18760672d2a278da666ff7a20de347
Austin Akers <austin.akers5@gmail.com>
2018-05-23 22:52:03 -0500

    created a more efficent post retrieval

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
-
+export const FETCH_POST = 'FETCH_POSTS';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
 const API_KEY = '?key=guccidude123';
 export function fetchPosts() {
@@ -20,4 +20,13 @@ export function createPost(values, callback) {
     type: CREATE_POST,
     payload: request
   };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  }
 }
