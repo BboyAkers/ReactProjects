894ec96b10232ff37d997240234f4182fdddd16e
Austin Akers <austin.akers5@gmail.com>
2020-05-07 20:13:01 -0500

    added babel routing and carousel

--- react-project/src/SearchParams.js
+++ react-project/src/SearchParams.js
@@ -1,45 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import pet, { ANIMALS } from '@frontendmasters/pet';
-import useDropdown from './useDropdown';
-import Results from './Results'
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+import Results from "./Results";
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
-  const [breeds, setBreeds] = useState([]);
-  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [breeds, updateBreeds] = useState([]);
   const [pets, setPets] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
 
   async function requestPets() {
     const { animals } = await pet.animals({
       location,
       breed,
       type: animal
-    })
+    });
+
+    console.log("animals", animals);
 
     setPets(animals || []);
   }
 
   useEffect(() => {
-    setBreeds([]);
-    setBreed("");
+    updateBreeds([]);
+    updateBreed("");
 
     pet.breeds(animal).then(({ breeds }) => {
       const breedStrings = breeds.map(({ name }) => name);
-      setBreeds(breedStrings);
+      updateBreeds(breedStrings);
     }, console.error);
-  }, [animal, setBreed, setBreeds]);
+  }, [animal]);
 
   return (
     <div className="search-params">
-      <h1>{location}</h1>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        requestPets();
-      }}>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" onChange={e => setLocation(e.target.value)} />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => updateLocation(e.target.value)}
+          />
         </label>
         <AnimalDropdown />
         <BreedDropdown />
@@ -47,7 +55,7 @@ const SearchParams = () => {
       </form>
       <Results pets={pets} />
     </div>
-  )
-}
+  );
+};
 
 export default SearchParams;
