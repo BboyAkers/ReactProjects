3ce2004c4fbde9bfca431190821b6e33c6d33624
Austin Akers <austin.akers5@gmail.com>
2023-10-06 17:40:10 -0700

    adding base setup for graphQL app

---
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import gql from 'graphql-tag';
+
+const SongList = () => {
+
+  const fetchSongList = () => {
+
+  }
+
+  return (
+    <div>
+      SongList
+    </div>
+  )
+
+}
+
+export default SongList;
