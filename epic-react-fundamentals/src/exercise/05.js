68ce99a7a36062ecec6e3679eaf5bb535b31492b
Austin Akers <austin.akers5@gmail.com>
2023-10-27 16:05:18 -0700

    added epic react fundamentals section

---
+++ epic-react-fundamentals/src/exercise/05.js
@@ -0,0 +1,31 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+// 🐨 add a className prop to each div and apply the correct class names
+// based on the text content
+// 💰 Here are the available class names: box, box--large, box--medium, box--small
+// 💰 each of the elements should have the "box" className applied
+
+// 🐨 add a style prop to each div so their background color
+// matches what the text says it should be
+// 🐨 also use the style prop to make the font italic
+// 💰 Here are available style attributes: backgroundColor, fontStyle
+
+const smallBox = <div>small lightblue box</div>
+const mediumBox = <div>medium pink box</div>
+const largeBox = <div>large orange box</div>
+
+function App() {
+  return (
+    <div>
+      {smallBox}
+      {mediumBox}
+      {largeBox}
+    </div>
+  )
+}
+
+export default App
