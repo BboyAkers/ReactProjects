6a77f77be80208abac484a0f62ea44dfc6c28fee
Austin Akers <austin.akers5@gmail.com>
2023-11-01 18:02:41 -0700

    updating excercise 6

--- epic-react-fundamentals/src/exercise/06.js
+++ epic-react-fundamentals/src/exercise/06.js
@@ -3,7 +3,7 @@
 
 import * as React from 'react'
 
-function UsernameForm({onSubmitUsername}) {
+function UsernameForm({ onSubmitUsername }) {
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
@@ -19,11 +19,19 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+
+  const usernameInputRef = React.useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const value = usernameInputRef.current.value
+    onSubmitUsername(value)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input id="usernameInput" ref={usernameInputRef} type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
