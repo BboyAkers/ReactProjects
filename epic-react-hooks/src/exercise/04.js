9caf7ff6ab0ca1c7b759bc12998be16dfdd0ba50
Austin Akers <austin.akers5@gmail.com>
2023-11-10 06:53:01 +0000

    added functionality to tictactoe game

--- epic-react-hooks/src/exercise/04.js
+++ epic-react-hooks/src/exercise/04.js
@@ -5,9 +5,16 @@ import * as React from 'react'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  const [squares, setSquares] = React.useState(() => JSON.parse(window.localStorage.getItem('squares') || Array(9).fill(null)))
+
+  React.useEffect(() => {
+    window.localStorage.setItem('squares', JSON.stringify(squares))
+  }, [squares])
 
   // 🐨 We'll need the following bits of derived state:
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
   // - nextValue ('X' or 'O')
   // - winner ('X', 'O', or null)
   // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
@@ -17,6 +24,12 @@ function Board() {
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
+    if(winner || squares[square]){
+      return
+    }
+    const squaresCopy = [...squares];
+    squaresCopy[square] = nextValue
+    setSquares(squaresCopy)
     // 🐨 first, if there's already a winner or there's already a value at the
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
@@ -35,7 +48,7 @@ function Board() {
 
   function restart() {
     // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
+    setSquares(Array(9).fill(null))
   }
 
   function renderSquare(i) {
@@ -49,7 +62,7 @@ function Board() {
   return (
     <div>
       {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
