8da9f344e835bc906ad784e84c9dacb68a5132d4
Austin Akers <austin.akers5@gmail.com>
2023-12-06 16:14:50 -0800

    finished base of all functionality

--- github-search-app/src/App.tsx
+++ github-search-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { EventHandler, useEffect, useState } from 'react';
 import './App.css'
 import CompanyIcon from './assets/icon-company.svg?react';
 import LocationIcon from './assets/icon-location.svg?react';
@@ -7,50 +8,162 @@ import SunIcon from './assets/icon-sun.svg?react';
 import TwitterIcon from './assets/icon-twitter.svg?react';
 import WebsiteIcon from './assets/icon-website.svg?react';
 
+interface GithubSearchFormProps {
+  initialUsername: string,
+  onSubmit: (newUsername: string) => void
+}
+
+interface GithubUserCardState {
+    status?: string | null,
+    githubUser?: object | null,
+    error?: string | null,
+}
+
+const fetchGithubUser = (name: string) => {
+
+  return window
+    .fetch(`https://api.github.com/users/${name}`)
+    .then(async response => {
+      const data = await response.json()
+      if(response.ok && data.login) {
+          return data;
+      }
+      return Promise.reject('User not found');
+    })
+}
+
+const GithubSearchForm = ({initialUsername, onSubmit}: GithubSearchFormProps) => {
+
+  const [username, setUsername] = useState(initialUsername)
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setUsername(event.target.value)
+  }
+
+  const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit(username);
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="relative max-w-3xl flex justify-between bg-white h-14 rounded-2xl p-2">
+      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+        <SearchIcon />
+      </div>
+      <input 
+        type="search"
+        placeholder="Search Github username..."
+        value={username}
+        onChange={handleChange}
+        className="w-full text-sm pl-8" />
+      <button className="bg-blue-500 px-3 rounded-xl text-white" disabled={!username.length}>Search</button>
+    </form>
+  )
+}
+
+const GithubUserCardDetails = ({githubUser}) => {
+  console.log(githubUser)
+  const { name, login, avatar_url, html_url, created_at, bio, followers, following, public_repos, location, twitter_username, blog, company } = githubUser;
+  return (
+    <div className="max-w-3xl bg-white h-[500px] rounded-2xl mt-8 p-8">
+      <div className="grid grid-cols-8">
+        <img className="col-span-3 rounded-full inline" src={avatar_url} alt="Github profile picture" />
+        <div className="ml-4 col-span-5 inline-block">
+          <h2 className="font-semibold">{name}</h2>
+          <a href={html_url} target="_blank" className="text-blue-500">@{login}</a>
+          <p className="text-sm">{created_at}</p>
+        </div>
+      </div>
+      <p className="inline-block mt-8 text-sm">{bio}</p>
+      <div className="grid grid-cols-3 text-center p-4 bg-slate-300 rounded-xl my-6">
+        <div>
+          <p className="text-sm">Repos</p>
+          <p className="font-bold">{public_repos}</p>
+        </div>
+        <div>
+          <p className="text-sm">Followers</p>
+          <p className="font-bold">{followers}</p>
+        </div>
+        <div>
+          <p className="text-sm">Following</p>
+          <p className="font-bold">{following}</p>
+        </div>
+      </div>
+      <div className="grid grid-cols-1">
+        <p><LocationIcon className="inline-block mr-5" />{location ? location : 'Not Available'}</p>
+        <p><WebsiteIcon className="inline-block mr-4" />{blog ? blog : 'Not Available'}</p>
+        <p><TwitterIcon className="inline-block mr-4" />{twitter_username ? twitter_username : 'Not Available'}</p>
+        <p><CompanyIcon className="inline-block mr-4" />{company ? company : 'Not Available'}</p>
+      </div>
+    </div>
+  )
+}
+
+
+const GithubUserCard = ({ username }) => {
+  const [state, setState] = useState<GithubUserCardState>({
+    status: 'idle',
+    githubUser: null,
+    error: null,
+  });
+
+  const { status, githubUser, error } = state;
+
+  useEffect(() => {
+    if (!username) {
+      return
+    }
+    setState({ status: 'pending' })
+    fetchGithubUser(username)
+    .then(
+      githubUser => {
+        setState({ status: 'resolved', githubUser })
+      },
+      error => {
+        setState({ status: 'rejected', error })
+      },
+    )
+  }, [username])
+
+ if(status == 'idle') {
+  return (
+    <h1 className='mt-10 text-center text-xl text-white'>Submit a github username</h1>
+  )
+ } else if( status === 'pending') {
+  return (
+    <h1 className='mt-10 text-center text-xl text-white'>Loading...</h1>
+  )
+ } else if( status === 'rejected') {
+  return (
+    <h1 className='mt-10 text-center text-xl text-white'>{error}</h1>
+  )
+} else if( status === 'resolved') {
+  return (
+    <GithubUserCardDetails githubUser={githubUser} />
+  )
+}
+
+
+
+  
+}
+
+
 function App() {
+const [username, setUsername] = useState('');
+
+const handleSubmit = (newUsername: string) => {
+  setUsername(newUsername);
+}
 
   return (
     <main className="bg-slate-500 h-screen p-6">
       <header>
         <h1>devfinder</h1>
+        <GithubSearchForm initialUsername={username} onSubmit={handleSubmit} />
       </header>
       <section>
-        <form className="max-w-3xl flex justify-between bg-white h-14 rounded-2xl p-2">
-          <input placeholder="Search Github username..." type="search" className="w-full" />
-          <button className="bg-blue-500 px-3 rounded-xl text-white">Search</button>
-        </form>
-
-        <div className="max-w-3xl bg-white h-[500px] rounded-2xl mt-8 p-8">
-          <div className="grid grid-cols-8">
-            <img className="col-span-3 rounded-full inline" src="https://placehold.co/100x100" alt="Github profile picture" />
-            <div className="ml-4 col-span-5 inline-block">
-              <h2 className="font-semibold">The Octocat</h2>
-              <a href="#" className="text-blue-500">@octocat</a>
-              <p className="text-sm">Joined 25 Jan 2011</p>
-            </div>
-          </div>
-            <p className="inline-block mt-8 text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.</p>
-            <div className="grid grid-cols-3 text-center p-4 bg-slate-300 rounded-xl mt-6">
-              <div>
-                <p className="text-sm">Repos</p>
-                <p className="font-bold">8</p>
-              </div>
-              <div>
-                <p className="text-sm">Followers</p>
-                <p className="font-bold">3938</p>
-              </div>
-              <div>
-                <p className="text-sm">Following</p>
-                <p className="font-bold">9</p>
-              </div>
-            </div>
-            <div className="grid grid-cols-1">
-              <p><LocationIcon className="inline-block mr-5" />San Francisco</p>
-              <p><WebsiteIcon className="inline-block mr-4" />https://github.blog</p>
-              <p><TwitterIcon className="inline-block mr-4" />Not Available</p>
-              <p><CompanyIcon className="inline-block mr-4" />@github</p>
-            </div>
-        </div>
+        <GithubUserCard username={username} />
       </section>
     </main>
   )
