7d342e043ad8978f97eb9db257cacc8adae850d1
Austin Akers <austinakers@Austins-MacBook-Pro.local>
2024-01-17 15:43:39 -0800

    created stand alone context

--- epic-react-advanced-react-hooks/src/exercise/02.js
+++ epic-react-advanced-react-hooks/src/exercise/02.js
@@ -1,5 +1,6 @@
 // useCallback: custom hooks
-// http://localhost:3000/isolated/final/02.js
+// 💯 make safeDispatch with useCallback, useRef, and useEffect
+// http://localhost:3000/isolated/final/02.extra-3.js
 
 import * as React from 'react'
 import {
@@ -9,6 +10,27 @@ import {
   PokemonInfoFallback,
   PokemonErrorBoundary,
 } from '../pokemon'
+
+function useSafeDispatch(dispatch) {
+  const mountedRef = React.useRef(false)
+
+  // to make this even more generic you should use the useLayoutEffect hook to
+  // make sure that you are correctly setting the mountedRef.current immediately
+  // after React updates the DOM. Even though this effect does not interact
+  // with the dom another side effect inside a useLayoutEffect which does
+  // interact with the dom may depend on the value being set
+  React.useEffect(() => {
+    mountedRef.current = true
+    return () => {
+      mountedRef.current = false
+    }
+  }, [])
+
+  return React.useCallback(
+    (...args) => (mountedRef.current ? dispatch(...args) : void 0),
+    [dispatch],
+  )
+}
 
 function asyncReducer(state, action) {
   switch (action.type) {
@@ -27,50 +49,57 @@ function asyncReducer(state, action) {
   }
 }
 
-function useAsync(asyncCallback, initialState) {
-  const [state, dispatch] = React.useReducer(asyncReducer, {
+function useAsync(initialState) {
+  const [state, unsafeDispatch] = React.useReducer(asyncReducer, {
     status: 'idle',
     data: null,
     error: null,
     ...initialState,
   })
 
-  React.useEffect(() => {
-    const promise = asyncCallback()
-    if (!promise) {
-      return
-    }
-    dispatch({ type: 'pending' })
-    promise.then(
-      data => {
-        dispatch({ type: 'resolved', data })
-      },
-      error => {
-        dispatch({ type: 'rejected', error })
-      },
-    )
-    // too bad the eslint plugin can't statically analyze this :-(
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [asyncCallback])
+  const dispatch = useSafeDispatch(unsafeDispatch)
 
-  return state
+  const { data, error, status } = state
+
+  const run = React.useCallback(
+    promise => {
+      dispatch({ type: 'pending' })
+      promise.then(
+        data => {
+          dispatch({ type: 'resolved', data })
+        },
+        error => {
+          dispatch({ type: 'rejected', error })
+        },
+      )
+    },
+    [dispatch],
+  )
+
+  return {
+    error,
+    status,
+    data,
+    run,
+  }
 }
 
 function PokemonInfo({ pokemonName }) {
-  const asyncCallback = React.useCallback(() => {
+  const {
+    data: pokemon,
+    status,
+    error,
+    run,
+  } = useAsync({
+    status: pokemonName ? 'pending' : 'idle',
+  })
+
+  React.useEffect(() => {
     if (!pokemonName) {
       return
     }
-    return fetchPokemon(pokemonName)
-  }, [pokemonName]);
-
-  const state = useAsync(
-    asyncCallback,
-    { status: pokemonName ? 'pending' : 'idle' },
-    [pokemonName],
-  )
-
-  const { data: pokemon, status, error } = state
+    run(fetchPokemon(pokemonName))
+  }, [pokemonName, run])
 
   switch (status) {
     case 'idle':
--- epic-react-advanced-react-hooks/src/exercise/03.js
+++ epic-react-advanced-react-hooks/src/exercise/03.js
@@ -4,22 +4,31 @@
 import * as React from 'react'
 
 // 🐨 create your CountContext here with React.createContext
-
+const CountContext = React.createContext();
 // 🐨 create a CountProvider component here that does this:
-//   🐨 get the count state and setCount updater with React.useState
-//   🐨 create a `value` array with count and setCount
-//   🐨 return your context provider with the value assigned to that array and forward all the other props
-//   💰 more specifically, we need the children prop forwarded to the context provider
+const CountProvider = (props) => {
+  const [count, setCount] = React.useState(0);
+  const value = [count, setCount];
+  return <CountContext.Provider value={value} {...props} />
+}
+
+const useCount = () => {
+  const context = React.useContext(CountContext);
+  if (!context) {
+    throw new Error(`useContext must be used within the CountProvider`);
+  }
+  return context;
+}
 
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const count = 0
+  const [count] = useCount();
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
+  const [, setCount] = useCount();
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -27,12 +36,10 @@ function Counter() {
 function App() {
   return (
     <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
   )
 }
