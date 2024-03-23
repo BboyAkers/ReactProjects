18b33b75618b833c80c7cb5b2b8817cb89a5d4e9
Austin Akers <austinakers@Austins-MacBook-Pro.local>
2024-03-23 08:53:38 -0700

    finished exercise 1

--- epic-react-testing-react-apps/src/__tests__/exercise/01.js
+++ epic-react-testing-react-apps/src/__tests__/exercise/01.js
@@ -2,7 +2,6 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-import ReactDOM from 'react-dom'
 import { act } from 'react-dom/test-utils'
 import { createRoot } from 'react-dom/client'
 import Counter from '../../components/counter'
@@ -12,38 +11,38 @@ import Counter from '../../components/counter'
 // Luckily, it's handled for you by React Testing Library :)
 global.IS_REACT_ACT_ENVIRONMENT = true
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  const div = document.createElement('div');
-  document.body.append(div);
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  ReactDOM.render(<Counter />, div);
-
-  const [decrement, increment] = div.querySelectorAll('button');
-  const message = div.firstChild.querySelector('div');
-
-  expect(message.textContent).toBe('Current count: 0');
-
-  increment.click();
-  expect(message.textContent).toBe('Current count: 1')
-  decrement.click();
-  expect(message.textContent).toBe('Current count: 0');
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
-  // 🐨 use createRoot to render the <Counter /> to the div
-  // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 act(() => increment.click()))
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 act(() => decrement.click()))
-  // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+beforeEach(() => {
+  document.body.innerHTML = ''
 })
 
-/* eslint no-unused-vars:0 */
+test('counter increments and decrements when the buttons are clicked', () => {
+  const div = document.createElement('div')
+  document.body.append(div)
+
+  const root = createRoot(div)
+  act(() => root.render(<Counter />))
+  const [decrement, increment] = div.querySelectorAll('button')
+  const message = div.firstChild.querySelector('div')
+
+  expect(message.textContent).toBe('Current count: 0')
+
+  const incrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0
+  })
+
+  act(() => increment.dispatchEvent(incrementClickEvent))
+  expect(message.textContent).toBe('Current count: 1')
+
+  const decrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 1
+  })
+
+  act(() => decrement.dispatchEvent(decrementClickEvent))
+  expect(message.textContent).toBe('Current count: 0')
+
+  div.remove()
+})
