d4bc613c8e4a5ece91e1949dd5c9b523dc088c03
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:31 -0700

    cleaned project and started rebuilding

--- book_list/src/reducers/index.js
+++ book_list/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import BooksReducer from "./reducer_books";
-import ActiveBook from "./reducer_active_book";
+import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
-  books: BooksReducer,
-  activeBook: ActiveBook
+  books: BooksReducer
 });
 
 export default rootReducer;
