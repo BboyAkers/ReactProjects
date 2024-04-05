c32826545c19da66d1e5f8453345e64898cb0584
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:39 -0700

    rendering image list and description

--- modernreactcourse/searchapp/src/components/App.js
+++ modernreactcourse/searchapp/src/components/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends Component {
 
-  onSearchSubmit(term) {
-    console.log(term)
+  state = { imageList: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+      params: { query: term },
+      headers: {
+        Authorization: `Client-ID <AccessID>`
+      }
+    });
+    this.setState({ imageList: response.data.results })
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '20px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        {this.state.imageList.map(image => {
+          return (
+            <p>{image.description}</p>
+          )
+        })}
       </div>
     )
   }
