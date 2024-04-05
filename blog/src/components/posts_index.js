4d90ee3cbb8cac400493cb5b8ab6cc05b396bee8
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:32 -0700

    rendered posts

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
   render () {
     return (
       <div>
-        Post Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
 }
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
 
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
