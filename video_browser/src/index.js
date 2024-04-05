7b3504c3be908742809a07fbc597b989ff3d0b9f
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:31 -0700

    created youtube fetch call and rendering video list

--- video_browser/src/index.js
+++ video_browser/src/index.js
@@ -1,15 +1,39 @@
-import _ from "lodash";
+
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
+
+//Components
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+
+//API Key
 const API_KEY = "";
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch(
+      {
+        key: API_KEY,
+        term: 'surfboards'
+      },
+      (videos) => {
+        this.setState({ videos });
+      }
+    );
+  }
+
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
