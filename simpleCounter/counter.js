66e2e3d8e4b1b2948b364bd66fbb2fb606b52b84
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:30 -0700

    updated and completed counter

--- simpleCounter/counter.js
+++ simpleCounter/counter.js
@@ -2,8 +2,9 @@
 class Button extends React.Component {
 	render() {
   	return(
-  	<button onClick={this.props.incrementCounter}>
-    	+1
+  	<button 
+    	onClick={()=>this.props.onClickFunction(this.props.incrementValue)}>
+    	+{this.props.incrementValue}
     </button>
   	);
   }
@@ -11,24 +12,27 @@ class Button extends React.Component {
 
 const Result = (props) => {
 	return (
-  <div>...</div>
+  <div>{props.counter}</div>
   );
 };
 
 class App extends React.Component {
 	state = { counter: 5 };
-  incrementCounter = () => {
-  	this.setState((prevState) => {
+  incrementCounter = (incrementValue) => {
+  	this.setState(( prevState) => {
     	return {
-      	counter: this.state.counter + 1
+      	counter: prevState.counter + incrementValue
       };
     })
   }
 	render() {
   	return (
     	<div>
-      	<Button onClickFunction={this.incrementCounter} />
-        <Result />
+      	<Button incrementValue={1} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={5} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={10} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={100} onClickFunction={this.incrementCounter} />
+        <Result counter={this.state.counter} />
       </div>
     );
   }
