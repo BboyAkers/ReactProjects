25a28d6650249a0593930ac65385140098513c29
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:31 -0700

    started weather app

--- weather/src/index.js
+++ weather/src/index.js
@@ -5,12 +5,12 @@ import { createStore, applyMiddleware } from "redux";
 import ReduxPromise from "redux-promise";
 
 import App from "./components/app";
-import reducers from "./reducers";
+// import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+// const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider>
     <App />
   </Provider>,
   document.querySelector(".container")
