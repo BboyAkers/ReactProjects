a25f9bf56cad023506a699da5e31765061579d6f
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:33 -0700

    finished creating hook

--- react-hooks-app/src/component/ResourceList.js
+++ react-hooks-app/src/component/ResourceList.js
@@ -1,24 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-class ResourceList extends React.Component {
-  state = { resources: [] };
-  async componentDidMount() {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
+   const fetchResource = async (resource) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
 
-    this.setState({resources: response.data});
-  }
-  async componentDidUpdate(prevProps) {
-    if(prevProps !== this.props.resource) {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-
-      this.setState({resources: response.data});
-    }
-  }
-  render() {
+    setResources(response.data);
+  };
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource])
     return (
-      <div>{this.state.resources.length}</div>
+      <div>{resources.length}</div>
     )
-  }
 }
 
 export default ResourceList; 
