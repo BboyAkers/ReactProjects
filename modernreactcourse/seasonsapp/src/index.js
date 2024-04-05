557ade063f67dbdedd349c49ff30a0d12777cf1d
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:38 -0700

    showing seasons and showing icons

--- modernreactcourse/seasonsapp/src/index.js
+++ modernreactcourse/seasonsapp/src/index.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// import SeasonDisplay from './SeasonDisplay';
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = { latitude: null, longitude: null }
+  state = { latitude: null, longitude: null, errorMessage: '' }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         const { coords } = position
         this.setState({ latitude: coords.latitude, longitude: coords.longitude })
       },
-      (error) => console.log(error)
+      (error) => {
+        this.setState({ errorMessage: error.message })
+      }
     );
   }
   render() {
-
-    return (
-      <div>
-        <h1>Latitude: {this.state.latitude}</h1>
-        <h1>Longitude: {this.state.longitude}</h1>
-      </div>
-    )
+    if (this.state.errorMessage && !this.state.latitude) {
+      return (
+        <div>
+          <h2>{this.state.errorMessage}</h2>
+        </div>
+      )
+    }
+    if (!this.state.errorMessage && this.state.latitude) {
+      return (
+        <div>
+          <SeasonDisplay lat={this.state.latitude} />
+        </div>
+      )
+    }
+    return <div>Loading!!!</div>
   }
 }
 
