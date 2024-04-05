9eeab1621eb0e56636c9c2f57f1b492d199ca38b
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:33 -0700

    added component card inside approval card and displays content

--- blog/src/components/ApprovalCard.js
+++ blog/src/components/ApprovalCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const ApprovalCard = (props) => {
-  console.log(props.children)
   return (
     <div className="ui card">
       <div className="content">{props.children}</div>
