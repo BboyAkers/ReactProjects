b006a8e43d4e260760ee9e89cd72c9f28970c2af
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:38 -0700

    refactored seasonDisplay

--- modernreactcourse/seasonsapp/src/SeasonDisplay.js
+++ modernreactcourse/seasonsapp/src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach!',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'It\'s cold outside',
+    iconName: 'snowflake'
+  }
+}
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
@@ -11,12 +22,12 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = ({ lat }) => {
   const season = getSeason(lat, new Date().getMonth())
-  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  const { text, iconName } = seasonConfig[season];
   return (
     <div>
-      <i className={`${icon} icon`} />
-      <h2>{season === 'winter' ? 'It is chilly outside' : 'Time to go to the pool'}</h2>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
+      <h2>{text}</h2>
+      <i className={`${iconName} icon`} />
     </div>
   )
 }
