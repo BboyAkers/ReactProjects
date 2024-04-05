9bb7ad83bf4d8dcd3d2c1d4c2602a6f499b2cb76
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:37 -0700

    added reach router

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { render } from "react-dom";
-
-import Pet from "./Pet";
+import { Router, Link } from '@reach/router'
+import Pet from './Pet';
 import SearchParams from "./SearchParams";
+import Details from './Details';
 
 
 const App = () => {
   return (
     <div>
-      <h1 id="">Adopt Me!</h1>
+      <header>
+        <Link to="/">
+          Adopt Me!
+        </Link>
+      </header>
       <Pet name="Abbey" animal="Dog" breed="schnauzer" />
       <Pet name="Rex" animal="Dog" breed="Pitbull" />
-      <SearchParams />
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   )
 };
--- react-project/src/Pet.js
+++ react-project/src/Pet.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-export default function Pet({ name, animal, breed }) {
+export default function Pet({ name, animal, breed, media, location, hero, id }) {
     return (
-        <div>
-            <h1>{name}</h1>
-            <h2>{animal}</h2>
-            <h2>{breed}</h2>
+      <a href={`/details/${id}`} className="pet">
+        <div className="image-container">
+          <img src={hero} alt={name} />
         </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+        </div>
+      </a>
     )
 };
