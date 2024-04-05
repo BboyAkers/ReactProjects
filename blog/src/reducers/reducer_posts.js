4f135be4cd19d1b950d73526bf51f51fb08f4ecd
Austin Akers <austin.akers5@gmail.com>
2024-04-04 20:29:32 -0700

    created a more efficent post retrieval

--- blog/src/reducers/reducer_posts.js
+++ blog/src/reducers/reducer_posts.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
 import { FETCH_POSTS } from '../actions';
+import { FETCH_POST } from '../actions';
 
 export default function(state ={}, action){
   switch(action.type) {
     case FETCH_POSTS:
-        return _.mapKeys(action.payload.data, 'id');
+      return _.mapKeys(action.payload.data, 'id');
+    case FETCH_POST:
+      const post = action.payload.data;
+      const newState = { ...state };
+      newState[post.id] = post;
+      return newState;
     
     default:
       return state;
