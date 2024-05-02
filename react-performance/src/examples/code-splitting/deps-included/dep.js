b93f8750cd70b877c67ee26d679cb8faaeb0bf58
Austin Akers <austin.akers5@gmail.com>
2024-05-02 16:58:07 -0700

    initial commit with eager loading for excersise 1

---
+++ react-performance/src/examples/code-splitting/deps-included/dep.js
@@ -0,0 +1,8 @@
+// http://localhost:3000/isolated/examples/code-splitting/deps-included/dep.js
+
+import * as React from 'react'
+
+function Dep() {
+  return <div>Hello from a dependency!</div>
+}
+export default Dep
---
+++ react-performance/src/examples/code-splitting/preloaded.js
@@ -0,0 +1,9 @@
+// http://localhost:3000/isolated/examples/code-splitting/preloaded.js
+
+import * as React from 'react'
+
+function Preloaded() {
+  return <div>Preloaded module</div>
+}
+
+export default Preloaded
