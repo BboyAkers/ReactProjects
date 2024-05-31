cc8cd37b97ef6a6d9fd46f5b592b117a9f8e8cdb
Austin Akers <austin.akers5@gmail.com>
2024-05-31 05:28:59 -0700

    setup color palette

--- multi-step-form/tailwind.config.js
+++ multi-step-form/tailwind.config.js
@@ -5,7 +5,36 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': {
+          light: '#ABBCFF', // Light Blue
+          DEFAULT: '#BEE2FD', // Sky Blue
+          dark: '#022959' // Denim
+        },
+        'grey': {
+          light: 'F8F9FF', // Very Light Grey
+          DEFAULT: '#D6D9E6', // Light Grey
+          dark: '#9699AA' // Grey
+        },
+        'orange': {
+          DEFAULT: '#FFAF7E'
+        },
+        'pink': {
+          DEFAULT: '#F9818E'
+        },
+        'purple': {
+          DEFAULT: '#483EFF'
+        },
+        'red': {
+          DEFAULT: '#EE374A'
+        },
+        'white': {
+          DEFAULT: '#FFFFFF',
+          dark: '#EFF5FF'
+        }
+      }
+    },
   },
   plugins: [],
 }
