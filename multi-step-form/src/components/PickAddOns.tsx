3a0415dc907dc6242a5e8bc2862560d0576a22c4
Austin Akers <austin.akers5@gmail.com>
2024-07-18 23:14:11 -0500

    updating pick add ons section

--- multi-step-form/src/components/PickAddOns.tsx
+++ multi-step-form/src/components/PickAddOns.tsx
@@ -5,25 +5,21 @@ const PickAddOns = () => {
     <>
       <h2 className="pb-2 text-2xl font-semibold">Pick add-ons</h2>
       <p className="mb-4 font-light text-grey-dark">Add-ons help enhance your gaming experience.</p>
-      <fieldset>
-        <div>
-          <AddOnItem
-            title="Online Service"
-            subtitle="Access to multiplayer games"
-            price={1}
-            isAnnualPricing={false} />
-          <AddOnItem
-            title="Larger storage"
-            subtitle="Extra 1TB of cloud save"
-            price={2}
-            isAnnualPricing={false} />
-          <AddOnItem
-            title="Customizable profile"
-            subtitle="Custom theme on your profile"
-            price={2}
-            isAnnualPricing={false} />
-        </div>
-      </fieldset>
+        <AddOnItem
+          title="Online Service"
+          subtitle="Access to multiplayer games"
+          price={1}
+          isAnnualPricing={false} />
+        <AddOnItem
+          title="Larger storage"
+          subtitle="Extra 1TB of cloud save"
+          price={2}
+          isAnnualPricing={false} />
+        <AddOnItem
+          title="Customizable profile"
+          subtitle="Custom theme on your profile"
+          price={2}
+          isAnnualPricing={false} />
     </>
   )
 }
