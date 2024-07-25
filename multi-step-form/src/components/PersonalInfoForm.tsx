c6da8fd61d2cc6086f0e3405a020d85f3e8a6d22
Austin Akers <austin.akers5@gmail.com>
2024-07-24 21:13:46 -0700

    added error highlighting for inputs

--- multi-step-form/src/components/PersonalInfoForm.tsx
+++ multi-step-form/src/components/PersonalInfoForm.tsx
@@ -17,7 +17,7 @@ const PersonalInfoForm = () => {
             {...register("name", { required: true })}
             type="text"
             placeholder="e.g. Stephen King"
-            className="block h-10 p-4 mb-4 text-base border-2 rounded border-gray w-72 focus:border-purple invalid:border-red focus:invalid:border-red"
+            className={`block h-10 p-4 mb-4 text-base border-2 rounded border-gray w-72 focus:outline-none ${errors.name ? "focus:border-red focus:ring-red" : "focus:border-purple focus:ring-purple"}`}
           />
         </label>
         <label className="text-xs text-blue-dark">
@@ -26,10 +26,10 @@ const PersonalInfoForm = () => {
             {errors.email && <span className="font-bold text-red">This field is required</span>}
           </span>
           <input
-            {...register("email", { required: true })}
+            {...register("email", { required: true})}
             type="email"
             placeholder="e.g. stephenking@lorem.com"
-            className="block h-10 p-4 mb-4 text-base border-2 rounded border-gray w-72 focus:border-purple"
+            className={`block h-10 p-4 mb-4 text-base border-2 rounded border-gray w-72 focus:outline-none ${errors.email ? "focus:border-red focus:ring-red" : "focus:border-purple focus:ring-purple"}`}
           />
         </label>
         <label className="text-xs text-blue-dark">
@@ -38,7 +38,7 @@ const PersonalInfoForm = () => {
             {...register("phone")}
             type="tel"
             placeholder="e.g. +1 234 567 890"
-            className="block h-10 p-4 mb-4 text-base border-2 rounded border-gray w-72 focus:border-purple"
+            className="block h-10 p-4 mb-4 text-base border-2 rounded border-gray w-72 focus:outline-none focus:border-purple focus:ring-purple invalid:border-red focus:invalid:border-red"
           />
         </label>
         </div>
