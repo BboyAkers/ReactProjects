d5016ce7dc6c7359ade89a1a6f55bf5845f5a52e
Austin Akers <austin.akers5@gmail.com>
2024-08-23 00:54:22 -0700

    fixed ci/cd pipeline errors

--- multi-step-form/src/components/FinishUp.tsx
+++ multi-step-form/src/components/FinishUp.tsx
@@ -1,10 +1,6 @@
+import React from "react";
 import { Link } from "react-router-dom";
-import { FormInfo } from "../App";
 import { useFormContext } from "react-hook-form";
-
-// type FinishUpProps = {
-//   formInfo: FormInfo;
-// };
 
 const FinishUp = () => {
   const { getValues } = useFormContext();
